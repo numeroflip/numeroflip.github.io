5a783dc56fea3e98f5455f91d8693c51a5f6d367
Aron Berenyi <aron.berenyi@tuta.io>
2024-07-03 11:14:22 +0200

    initial layout and content

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const Config = {
+  linkedInUrl: "https://www.linkedin.com/in/aron-berenyi/",
+  githubUrl: "https://github.com/numeroflip",
+  emailUrl: "mailto:aron.berenyi@tuta.io",
+  name: "Aron Berenyi",
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+
+const primaryColor = colors.sky;
+const secondaryColor = colors.amber;
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat Variable", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        primary: primaryColor,
+        secondary: secondaryColor,
+        text: primaryColor[950],
+      },
+      boxShadow: {
+        straight: `8px 8px 0px currentColor`,
+      },
+    },
+  },
+  plugins: [],
+};
